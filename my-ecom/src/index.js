e134e558a3461ab76fb20ef88fd14fad053bf931
omkar <borudeomkar41@gmail.com>
2021-04-01 00:01:47 +0530

    ADD

--- my-ecom/src/index.js
+++ my-ecom/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-const rootElement = document.getElementById("root");
+import React from "react";
+
+import ReactDOM from "react-dom";
+import App from "./App";
+import { DataProvider } from "./Comenents/Context/DataProvider";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataProvider>
+      <App />
+    </DataProvider>
   </React.StrictMode>,
-  rootElement
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
